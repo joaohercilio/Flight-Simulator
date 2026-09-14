c580534d1dd20a65ec1d85b576ced890ac129976
João Hercilio <joaoherciliozucchi@hotmail.com>
2026-09-14 11:43:59 -0300

    Changed default aircraft vales

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import argparse
 import pathlib
 import sys
 
-DEFAULT_CASE = pathlib.Path(__file__).resolve().parent / "cases" / "mushu"
+DEFAULT_CASE = pathlib.Path(__file__).resolve().parent / "cases" / "default_plane"
 
 
 def cmd_gui(args) -> None:
